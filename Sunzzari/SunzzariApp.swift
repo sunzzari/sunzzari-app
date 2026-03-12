8e000e6b13285b3f2eaf3bedc68f50ad35a8f109
Elisa Fazzari <elisafazzari@ucla.edu>
2026-03-12 11:05:23 -0700

    Initial Commit

---
+++ Sunzzari/SunzzariApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunzzariApp.swift
+//  Sunzzari
+//
+//  Created by Elisa Fazzari on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct SunzzariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
